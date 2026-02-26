b6e87a2c3947ededa71477d2b24dd6a42a6bf499
benmb <benmb@BENSCOMPUTER>
2026-02-25 21:46:36 -0500

    Add project files.

---
+++ P6.1.cpp
@@ -0,0 +1,50 @@
+/*
+File: P6.1
+Author: Ben Best
+Date: 2026-02-25
+
+Description: Filled and hollow square to the terminal
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int squareRow = 0;
+    int squareColumn = 0;
+
+    cout << "Enter a number for the length of your square: ";
+    cin >> n;
+
+    if (n < 3)
+    {
+        cout << "length must be at least 3." << endl;
+        return 0;
+    }
+
+    for (squareRow = 1; squareRow <= n; squareRow++)
+    {
+        //Filled square loop
+        for (squareColumn = 1; squareColumn <= n; squareColumn++)
+        {
+            cout << "*";
+        }
+
+        cout << " ";  //Space between the two squares
+
+        //Hollow square loop
+        for (int col = 1; col <= n; col++)
+        {
+            if (squareRow == 1 || squareRow == n || col == 1 || col == n)
+                cout << "*";
+            else
+                cout << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
